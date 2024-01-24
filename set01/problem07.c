6bb666b0560e23f0508db1596f76e4577f60db63
Yashwanth <yashwanthr.bsc@rvu.edu.in>
2024-01-24 10:19:13 +0530

    test

---
+++ set01/problem07.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int input_n()
+ {
+    int n;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+    return n;
+}
+
+int sum_n_nos(int n)
+ {
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+     {
+        sum += i;
+    }
+    return sum;
+}
+
+void output(int n, int sum)
+ {
+    printf("The sum of natural numbers up to %d is: %d\n", n, sum);
+}
+
+int main()
+ {
+    int n, sum;
+    n = input_n();
+    sum = sum_n_nos(n);
+    output(n, sum);
+
+    return 0;
+}
