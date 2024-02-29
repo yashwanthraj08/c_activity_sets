45b4bf4aa37a92b10d7026b6a98c5a1caa19b6a0
yashwanthr <yashwanthr.bsc23@rvu.edu.in>
2024-02-29 13:23:27 +0530

    test

--- set03/problem02.c
+++ set03/problem02.c
@@ -30,8 +30,6 @@ int is_triangle(float x1, float y1, float x2, float y2, float x3, float y3)
     float area = 0.5 * ((x1 * (y2 - y3)) + (x2 * (y3 - y1)) + (x3 * (y1 - y2)));
     return (area != 0);
 }
-
-// Function to output the result
 void output(float x1, float y1, float x2, float y2, float x3, float y3, int result)
  {
     if (result)
